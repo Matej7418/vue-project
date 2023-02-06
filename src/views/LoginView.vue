64146ac4b7e1524a54b881924f659712a25a24e6
Matej7418 <matejsl404@gmail.com>
2023-02-06 23:39:10 +0100

    1

---
+++ src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <Auth
+      :providers="['google', 'apple']"
+      @signInWithPassword="creds => supabase.auth.signInWithPassword(creds)"
+      @signUp="creds => supabase.auth.signUp(creds)"
+      @signInWithOAuth="provider => supabase.auth.signInWithOAuth(provider)"
+  />
+</template>
+
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from 'vue-auth-ui'
+
+const supabase = createClient(
+    'my-project-url',
+    'my-anon-key'
+)
+</script>
