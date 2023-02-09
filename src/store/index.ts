c6a05c39b3734cc5a89d8cb6a1c421899002c849
Matej7418 <matejsl404@gmail.com>
2023-02-09 20:07:08 +0100

    test

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,32 @@
-import { createStore } from 'vuex'
-import { ref } from 'vue'
+import { createStore } from 'vuex';
+import router from '../router';
+import { supabase } from '@/supabase.js';
 
 export default createStore({
   state: {
-    session: ref()
+    session: null
   },
   getters: {
   },
   mutations: {
     setSession(state, session) {
-      state.session.value = session;
+      state.session = session;
     }
   },
   actions: {
+    async signUpAction({dispatch}, form) {
+      try {
+        const { error } = await supabase.auth.signUp({
+          email: form.email,
+          password: form.password,
+        });
+        if (error) throw error;
+        alert("You've been registered successfully");
+        await dispatch("signInAction", form)
+      } catch (error) {
+        alert(error.error_description || error.message);
+      }
+    }
   },
   modules: {
   }
